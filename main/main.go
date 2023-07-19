6af06130f6aa45ba140530bb1753edc2cd47cd6e
MohamadAmin Seydi <mr.seadi@gmail.com>
2023-07-20 01:54:50 +0330

    Update main.go

--- main/main.go
+++ main/main.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	// "net/http"
-	"github.com/drdesignx/url-shortener/model"
+	"fmt"
+
+	"github.com/drdesignx/linkly/model"
 )
 
-var redirectMap = map[string]string{
-	"/gg" : "https://google.com",
-	"/twit" : "https://twitter.com",
-	"/gith" : "https://github.com",
-	"/digi" : "https://digikala.com",
-}
-
-// func redirectHandler(w http.ResponseWriter, r *http.Request) {
-// 	// redirectURL , exists := redirectMap[r.URL.Path]
-// 	// if exists {
 // 	// 	http.Redirect(w, r, redirectURL, http.StatusFound)
-// 	// 	return
-// 	// }
-// 	// http.NotFound(w,r)
-// }
 
 func main() {
+	fmt.Println("Start...")
 	model.Setup()
-
 
 }
